29a36cc672ff32a17e8396a8ab7512832391b654
KUYATE UMESH SHIVAJI <202401080042@mitaoe.ac.in>
2025-06-17 14:42:37 +0530

    Create ternaryOperator.java

---
+++ ternaryOperator.java
@@ -0,0 +1,10 @@
+public class ternaryOperator {
+    public static void main(String[] args) {
+        int number = 9;
+
+        String result = (number % 2 == 0) ? "Even" : "Odd";
+
+        System.out.println(number + " is " + result);
+    }
+    
+}
