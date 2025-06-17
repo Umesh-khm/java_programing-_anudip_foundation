eec1d4029308f680615947c873d62c1d1bd834c7
KUYATE UMESH SHIVAJI <202401080042@mitaoe.ac.in>
2025-06-17 14:47:35 +0530

    Create instance.java

---
+++ instance.java
@@ -0,0 +1,9 @@
+public class Instance {
+    public static void main(String[] args) {
+        Integer num = 100;
+        boolean result;
+
+        result = num instanceof Integer;
+        System.out.println("Is num an instance of Integer? " + result);
+    }
+}
