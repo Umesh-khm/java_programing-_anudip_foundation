be894ca0d736749eebc1ef4d336ea89b429b48e3
KUYATE UMESH SHIVAJI <202401080042@mitaoe.ac.in>
2025-06-17 14:36:56 +0530

    Create operator.java

---
+++ operator.java
@@ -0,0 +1,11 @@
+public class operator {
+    public static void main(String[] args){
+        int a = 2;
+        int b = 3;
+        
+        a+=b;
+        System.out.println(a);
+
+    }
+    
+}
